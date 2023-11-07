99cdf970c6df405b6fa9e3839319b684c9996a51
tassbash <92358385+tassbash@users.noreply.github.com>
2023-11-07 14:04:19 -0500

    added jump

---
+++ Platformer Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Movement Variables
+    Rigidbody2D rb; //create reference for rigidbody bc jump requires physics
+    public float jumpForce; //the force that will be added to the vertical component of player's velocity
+    public float speed;
+
+
+    //Ground Check Variables
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public bool isGrounded;
+
+    SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, .5f, groundLayer);
+        
+        Vector3 newPosition = transform.position;
+        Vector3 newScale = transform.localScale;
+        float currentScale = Mathf.Abs(transform.localScale.x);
+
+        if(Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition.x -= speed;
+            newScale.x = -currentScale;
+        }
+
+        if(Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition.x += speed;
+            newScale.x = currentScale;
+        }
+
+        if((Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+
+        transform.position = newPosition; 
+        transform.localScale = newScale;
+    }
+}
