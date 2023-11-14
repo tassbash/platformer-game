77da6c92c6210fab3da3785f9304585832d955cc
tassbash <92358385+tassbash@users.noreply.github.com>
2023-11-14 13:48:18 -0500

    added animator

--- Platformer Game/Assets/Scripts/PlayerController.cs
+++ Platformer Game/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Animation variables
+    Animator anim;
+    public bool moving = false;
+    
     //Movement Variables
     Rigidbody2D rb; //create reference for rigidbody bc jump requires physics
     public float jumpForce; //the force that will be added to the vertical component of player's velocity
@@ -22,6 +26,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -37,12 +42,14 @@ public class PlayerController : MonoBehaviour
         {
             newPosition.x -= speed;
             newScale.x = -currentScale;
+            moving = true;
         }
 
         if(Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
         {
             newPosition.x += speed;
             newScale.x = currentScale;
+            moving = true;
         }
 
         if((Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded == true)
@@ -50,7 +57,12 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
+        if(Input.GetKeyUp("a") || Input.GetKeyUp("d") || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            moving = false;
+        }
 
+        anim.SetBool("isMoving", moving);
         transform.position = newPosition; 
         transform.localScale = newScale;
     }
